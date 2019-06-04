820742f68bcd62510adf659f848c0c1aafc1153e
gitwho <2387639696@qq.com>
2019-06-04 22:01:31 +0800

    add

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,8 +1,28 @@
 import React, {Component} from 'react'
+import {CSSTransition} from 'react-transition-group'
 
 import "../../static/iconfont/iconfont.css";
 import './index.less'
 export default class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      focused: false
+    };
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+    this.handleInputBlur = this.handleInputBlur.bind(this);
+  }
+  handleInputFocus() {
+    this.setState({
+      focused: true
+    })
+  }
+  handleInputBlur() {
+    this.setState({
+      focused: false
+    })
+  }
+
   render() {
     return (
       <div className="header">
@@ -10,10 +30,21 @@ export default class Header extends Component {
         <div className="itemBox">
           <div className="item">首页</div>
           <div className="item">下载App</div>
+
           <div className="item">
-            <input className="search" placeholder="搜索" />
-            <a>
-              <i className="search icon-search"></i>
+            <CSSTransition
+              in={this.state.focused}
+              timeout={300}
+              classNames="slide"
+            >
+              <input className={`search ${this.state.focused ? "focused":''}`} placeholder="搜索" 
+                onFocus={this.handleInputFocus}
+                onBlur={this.handleInputBlur}
+              />
+            </CSSTransition>
+            
+            <a className={this.state.focused ? "focused":''}>
+              <i className="iconfont icon-search"></i>
             </a>
           </div>
           <div className="item">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import "./static/global.less"
 import "./static/iconfont/iconfont.css"
 
 
